0ea8f09c3fe0f589ddfe36626dc3c957f3ede02d
joaomaxdev <r15.joaomarques@gmail.com>
2025-05-19 17:59:33 -0300

    add renders

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,65 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header'
-import { StyleSheet, Text, View } from 'react-native';
+import Balance from '../../components/Balance';
+import Moviments from '../../components/Moviments';
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto conta luz',
+    value: '300,90',
+    date: '17/09/2022',
+    type: 0 //despesas
+  },
+  {
+    id: 1,
+    label: 'Pix Cliente X',
+    value: '2.500,00',
+    date: '20/09/2022',
+    type: 1 //receita
+  },
+  {
+    id: 1,
+    label: 'Salário',
+    value: '7.200,00',
+    date: '22/09/2022',
+    type: 1 //receita
+  },
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="Joao Max" />
 
+      <Balance saldo="9.250,90" gastos="-527,00" />
+
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList
+        style={styles.list}
+        data={list}
+        keyExtractor={(item) => String.id}
+        showsVerticalScrollIndication={false}
+        renderItem={({ item }) => <Moviments data={item} />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
   },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14
+  }
 });
